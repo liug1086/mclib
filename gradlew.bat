92a785a9a0f57fd887ffe4383f822ea7a5ab4a54
liug1086 <138269022+liug1086@users.noreply.github.com>
2025-01-05 19:35:24 +0800

    Update gradlew.bat script for Windows to ensure proper Gradle startup

--- gradlew.bat
+++ gradlew.bat
@@ -28,7 +28,6 @@ echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PA
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
-
 goto fail
 
 :findJavaFromJavaHome
@@ -42,11 +41,10 @@ echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
-
 goto fail
 
 :init
-@rem Get command-line arguments, handling Windowz variants
+@rem Get command-line arguments, handling Windows variants
 
 if not "%OS%" == "Windows_NT" goto win9xME_args
 if "%@eval[2+2]" == "4" goto 4NT_args
@@ -81,7 +79,7 @@ if "%ERRORLEVEL%"=="0" goto mainEnd
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+if not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
